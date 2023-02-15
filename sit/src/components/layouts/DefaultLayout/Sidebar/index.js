08f2e3c97803399245224cf3b1c4cd01e49710a0
Zen <64142347+zensdev@users.noreply.github.com>
2023-02-15 21:25:34 +0700

    fix hover icon animation button

--- sit/src/components/layouts/DefaultLayout/Sidebar/index.js
+++ sit/src/components/layouts/DefaultLayout/Sidebar/index.js
@@ -1,10 +1,28 @@
 import classNames from "classnames/bind";
 import style from "./Sidebar.module.scss";
 
+import Button from "~/components/Button";
+
 const cx = classNames.bind(style);
 
 function Sidebar() {
-  return <div className={cx("wrapper")}></div>;
+  return (
+    <div className={cx("wrapper")}>
+      <Button
+        text
+        leftIcon={
+          <lord-icon
+            src="https://cdn.lordicon.com/gqzfzudq.json"
+            trigger="hover"
+            colors="primary:#ed7966,secondary:#030e12"
+            style={{ width: "250px", height: "250px" }}
+          ></lord-icon>
+        }
+      >
+        Câu hỏi
+      </Button>
+    </div>
+  );
 }
 
 export default Sidebar;
